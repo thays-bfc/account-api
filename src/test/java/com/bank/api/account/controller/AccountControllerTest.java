8056360f89c2cf92d860c3690eea382741990f4f
Thays Barbara <thaysbarbara@hotmail.com>
2021-07-25 22:24:28 -0300

    Controller validation test

--- src/test/java/com/bank/api/account/controller/AccountControllerTest.java
+++ src/test/java/com/bank/api/account/controller/AccountControllerTest.java
@@ -1,7 +1,43 @@
 package com.bank.api.account.controller;
 
+import com.bank.api.account.service.AccountService;
+import com.bank.api.account.util.message.SystemConstants;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = {AccountController.class})
 public class AccountControllerTest {
-    // Should create account for new customer
-    // Should not create new account for existing customer
-    // Should not accept invalid Customer information
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    void shouldReturnBadRequestWhenInvalidCPF() throws Exception {
+        URI uri = new URI("/account");
+        String json = "{\n" +
+                "    \"name\": \"José Araujo\",\n" +
+                "    \"cpf\": \"656.520.450-005\"\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+
 }
--- src/test/java/com/bank/api/account/controller/AccountTransactionControllerTest.java
+++ src/test/java/com/bank/api/account/controller/AccountTransactionControllerTest.java
@@ -1,7 +1,42 @@
 package com.bank.api.account.controller;
 
+import com.bank.api.account.service.AccountTransactionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = {AccountTransactionController.class})
 public class AccountTransactionControllerTest {
-    // Should deposit money into account
-    // Should withdraw money from account
-    // Should transfer money of between valid accounts
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AccountTransactionService accountTransactionService;
+
+    @Test
+    void shouldReturnBadRequestWhenDepositNegativeValue() throws Exception {
+        URI uri = new URI("/transaction/deposit");
+        String json = "{\n" +
+                "    \"accountCode\": 1,\n" +
+                "    \"depositValue\": -1\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+
 }
